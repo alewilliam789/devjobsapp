42ec1a0c81536ce3a19b5d5aaea991b356e6800d
alewilliam789 <alewilliam789@gmail.com>
2023-07-12 08:45:34 -0700

    Add React-Router and Create a Browser Router
    Base route works, but working on the children elements/routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 
-import './App.css'
-import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom';
 
+
+
+
+import Header from './components/Header/Header'
+import Jobs from './components/Jobs/Jobs'
 import SearchBar from './components/SearchBar/SearchBar'
+
+import './App.css'
 
 function App() {
 
   return (
     <>
     <Header />
-    <main className={`flex-column justify-center`}>
-      <SearchBar />
-    </main>
+    <Outlet />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
 import App from './App.tsx'
+import Jobs from './components/Jobs/Jobs.tsx';
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (<App />),
+    children: [
+      {
+        index: true,
+        element: <Jobs />
+      },
+      {
+        path: "jobs:jobId",
+        element: (
+          <>
+            <div>Here's a job</div>
+          </>
+        )
+        
+      }
+    ]
+  }
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
