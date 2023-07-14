9f2872637bec60158c9740a345d9cc63995a5114
alewilliam789 <alewilliam789@gmail.com>
2023-07-14 11:20:33 -0700

    Wire load jobs into JobCards and display in grid
    Works with all screen sizes. Next is adding the ability to
    only load a certain amount of data

--- src/components/Jobs/Jobs.tsx
+++ src/components/Jobs/Jobs.tsx
@@ -1,17 +1,59 @@
+import { useEffect } from 'react';
+import { useQueryClient } from "@tanstack/react-query";
+
+
 import JobCard from "../JobCard/JobCard";
 import SearchBar from "../SearchBar/SearchBar";
 
-import styles from './styles.module.css'
+
+import useJobs from "../../hooks";
+
+import { JobData } from "../../types";
+
+
+import styles from './styles.module.css';
 
 
 
 
+function isUndefined(data : JobData[] | undefined){
+    if(typeof data != "undefined"){
+        return data
+    }
+    return []
+};
 
 export default function Jobs(){
+
+    const { data } = useJobs();
+
+    const queryClient = useQueryClient()
+
+    useEffect(()=>{
+        console.log(innerHeight)
+    },[innerHeight])
+
+
+    const definedData = isUndefined(data);
+
+    
+    const jobList = definedData.map((job)=> {
+        queryClient.setQueryData(['job',{id: job.id}], job)
+            return(
+                <li className={styles['jobs-card']} key={job.id}>
+                    <JobCard job={job} />
+                </li>
+            )
+    })
+
+
     return (
         <>
-            <section className={`flex-column justify-center`}>
+            <section className={`flex-column justify-center ${styles.jobs}`}>
                 <SearchBar />
+                <ul className={`${styles['jobs-cardlist']}`}>
+                    {jobList}
+                </ul>
             </section>
         </>
     )
