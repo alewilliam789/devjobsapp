a0bfbc7b7db569a89d4320601d33751a18f12393
alewilliam789 <alewilliam789@gmail.com>
2023-07-17 08:02:27 -0700

    Add the correct header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import ThemeSelector from '../ThemeSelector/ThemeSelector';
 
 
 import HeaderBackground from '../../assets/desktop/bg-pattern-header.svg';
+import {ReactComponent as Logo} from '../../assets/desktop/logo.svg';
 
 
 
@@ -15,7 +16,7 @@ export default function Header(){
     return (
         <header className={styles.header} style={{backgroundImage: `url(${HeaderBackground})`}}>  
             <section className={` flex justify-between ${styles['header-content']}`}>
-                <h1 className={`${styles["header-title"]}`}>devjobs</h1>
+                <Logo />
                 <ThemeSelector />
             </section>
         </header>
