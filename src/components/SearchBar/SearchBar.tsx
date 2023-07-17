ab53e2ff37f63d7644246270c13186f0e290ec1d
alewilliam789 <alewilliam789@gmail.com>
2023-07-17 09:11:27 -0700

    Add dynamic resizing to the SeachBar for mobile
    This allows the SearchBar to replace the original inputs with the
    mobile layout. It also adds media-queries to change the layout
    of the SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,26 @@
-import {useForm} from 'react-hook-form'
-import SearchInput from '../SearchInput/SearchInput';
+import { useForm } from 'react-hook-form';
+
+
+import { useScreenWidth } from '../../hooks';
+
 
 import styles from './styles.module.css';
 
 
-
-import {ReactComponent as SearchIcon} from '../../assets/desktop/icon-search.svg'
-import {ReactComponent as LocationIcon} from '../../assets/desktop/icon-location.svg'
+import SearchInput from '../SearchInput/SearchInput';
 import Button from '../Button/Button';
 
+
+import {ReactComponent as SearchIcon} from '../../assets/desktop/icon-search.svg';
+import {ReactComponent as LocationIcon} from '../../assets/desktop/icon-location.svg';
+import {ReactComponent as MobileSearchIcon} from '../../assets/mobile/icon-search-white.svg';
+import {ReactComponent as FilterIcon} from '../../assets/mobile/icon-filter.svg';
+
+
+
 export default function SearchBar(){
+
+    const { width } = useScreenWidth();
     
     const {register, handleSubmit} = useForm(
         {
@@ -30,21 +41,40 @@ export default function SearchBar(){
                 console.log(data)
         }
 
+        function SearchBarSwitcher(){
+
+            if(width < 729){
+                return (
+                    <>
+                        <div className={`flex align-center justify-center ${styles["searchbar-search"]}`}>
+                            <FilterIcon />
+                            <Button buttonType='submit' Icon={MobileSearchIcon} handleClick={undefined} placeholderText={undefined} size={{width:"45px",height:"45px"}}/>
+                        </div>
+                    </>
+                )
+            }
+            return (
+                <>
+                    <div>
+                    <SearchInput register={register} searchParam={"jobLocation"}  Icon={LocationIcon} placeholder="Filter by location..." />
+                    </div>
+                    <div className={`flex justify-between align-center ${styles["searchbar-search"]}`}>
+                        <div className={`flex align-center ${styles['searchbar-full-time']}`}>
+                            <input id="full-time" className={`${styles["searchbar-checkbox"]}`} type="checkbox" {...register("fullTime")} />
+                            <label htmlFor="full-time" className={`${styles["searchbar-ft-text"]}`}>Full Time</label>
+                        </div>
+                        <Button buttonType='submit' placeholderText='Search' handleClick={undefined} />
+                    </div>
+                </>
+            )
+        }
+
         return (
             <form id='job' className={`${styles['searchbar']}`} onSubmit={handleSubmit(onSubmit)}>
                 <div>
-                <SearchInput register={register} searchParam={"jobDescriptor"} Icon={SearchIcon} placeholder="Enter desired job..." />
+                <SearchInput register={register} searchParam={"jobDescriptor"} Icon={ (width >= 768) ? SearchIcon : null} placeholder="Enter desired job..." />
                 </div>
-                <div>
-                <SearchInput register={register} searchParam={"jobLocation"}  Icon={LocationIcon} placeholder="Filter by location..." />
-                </div>
-                <div className={`flex justify-between align-center ${styles["searchbar-search"]}`}>
-                    <div className={`flex align-center ${styles['searchbar-full-time']}`}>
-                        <input id="full-time" className={`${styles["searchbar-checkbox"]}`} type="checkbox" {...register("fullTime")} />
-                        <label htmlFor="full-time" className={`${styles["searchbar-ft-text"]}`}>Full Time</label>
-                    </div>
-                    <Button buttonType='submit' placeholderText='Search' handleClick={undefined}/>
-                </div>
+                <SearchBarSwitcher />
             </form>
 
         )
