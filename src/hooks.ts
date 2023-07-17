637032dfcfecc13f18a2df0b6b58735cdc6a0676
alewilliam789 <alewilliam789@gmail.com>
2023-07-17 07:22:25 -0700

    Create a hook to dynamically output window width

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,13 @@
+import { useState, useEffect } from 'react';
 import {useQuery} from '@tanstack/react-query';
-import { JobData } from './types';
+
+
 import fetchJobs from './API';
+
+
+import { JobData } from './types';
+
+
 
 
 
@@ -9,4 +16,26 @@ import fetchJobs from './API';
 
 export function useJobs(){
     return useQuery<JobData[], Error>(['job'],()=>fetchJobs())
+}
+
+export function useScreenWidth(){
+    function getWindowWidth() {
+        const { innerWidth: width} = window;
+        return {
+          width
+        };
+      }
+      
+    const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+    
+    useEffect(() => {
+        function handleResize() {
+        setWindowWidth(getWindowWidth());
+        }
+    
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    
+    return windowWidth;
 }
