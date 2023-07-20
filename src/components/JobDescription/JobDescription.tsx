035ce6cedc78c4186836fe6f8e66f5169290e15e
alewilliam789 <alewilliam789@gmail.com>
2023-07-20 09:14:40 -0700

    Add a header to the JobDescription component
    Will also query data using the custom useJob hook

--- src/components/JobDescription/JobDescription.tsx
+++ src/components/JobDescription/JobDescription.tsx
@@ -1,15 +1,45 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+
+
+import { useJob } from "../../hooks";
+import { JobData } from "../../types";
+import Icon from "../Icon/Icon";
+
+
+import styles from './styles.module.css';
+
+
+
+
 
 export default function JobDescription(){
 
 
     const { jobId } = useParams();
 
+    const { data } = useJob(jobId ? jobId : "");
+
+
+    function JobHeader(){
+        return (
+            <>
+                <div className={` flex ${styles.jobdescription}`}>
+                <header className={`flex justify-start align-center ${styles["jobdescription-header"]}`}>
+                    <Icon iconURL={data ? data.logo : ""} iconType="description" bgColor={data ? data.logoBackground : "white"} iconSize={{width:"81px", height:"max-content"}} 
+                        logoSize={{width:"140px",height:"140px"}} borderRad="0px"/>
+                </header>
+                </div>
+            </>
+        )
+    }
+
+
+
     return (
         <>
-            <div>
-                {jobId}
-            </div>
+            <section>
+                <JobHeader />
+            </section>
         </>
     )
 }
