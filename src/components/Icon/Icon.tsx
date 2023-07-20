d2c68816a9591748afa9bf106f57c87500161af8
alewilliam789 <alewilliam789@gmail.com>
2023-07-20 09:12:05 -0700

    Allow the Icon component to be fully stylable
    Previously was just styled for the JobCard, but can now be used for
    any component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,18 +1,27 @@
 import styles from './styles.module.css'
 
+interface Size {
+    height: string,
+    width: string
+}
+
 interface IconProps {
     bgColor: string,
     iconURL: string
+    iconSize: Size
+    logoSize: Size
+    borderRad: string
+    iconType: string
 }
 
 
 
-export default function Icon({bgColor, iconURL} : IconProps){
+export default function Icon({bgColor, iconURL, iconSize, logoSize, borderRad, iconType} : IconProps){
 
 
     return (
-            <div className={styles.icon} style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor : `${bgColor}`, height : '50px', width : '50px', borderRadius: "15px"}}>
-                <img src={iconURL}/>
+            <div className={styles[`icon-${iconType}`]} style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor : `${bgColor}`, height : `${logoSize.height}`, width : `${logoSize.width}`, borderRadius: `${borderRad}`}}>
+                <img height={iconSize.height} width={iconSize.width} src={iconURL}/>
             </div>
     )
 
